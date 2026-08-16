a15bf617a71d5dd02947e136585c1c6c60d6f1ee
Your Name <abdulraffay854@gmail.com>
2026-08-17 02:12:01 +0500

    migrate6

--- main.py
+++ main.py
@@ -41,7 +41,10 @@ with engine.connect() as conn:
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+   allow_origins=[
+    "http://localhost:3000",
+    "https://hvac-frontend-gamma.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
